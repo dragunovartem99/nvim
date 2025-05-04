8b36d9b2b66e0822f1e0c86f00dbebb8992575a0
Artem Dragunov <dragunovartem99@gmail.com>
2025-05-04 16:47:33 +0300

    Improve LSP bindings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,37 +7,23 @@ return {
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("custom-lsp-attach", { clear = true }),
 			callback = function(event)
-				local map = function(keys, func, desc, mode)
+				local builtin = require("telescope.builtin")
+				local map = function(keys, method_name, desc, mode)
 					mode = mode or "n"
-					vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					vim.keymap.set(
+						mode,
+						keys,
+						builtin[method_name],
+						{ buffer = event.buf, desc = "LSP: " .. desc }
+					)
 				end
 
-				map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-				map(
-					"gD",
-					require("telescope.builtin").lsp_type_definitions,
-					"[G]oto Type [D]efinition"
-				)
-				map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-				map(
-					"gI",
-					require("telescope.builtin").lsp_implementations,
-					"[G]oto [I]mplementation"
-				)
-
-				map("<leader>r", vim.lsp.buf.rename, "[R]ename")
-				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
-
-				map(
-					"<leader>ds",
-					require("telescope.builtin").lsp_document_symbols,
-					"[D]ocument [S]ymbols"
-				)
-				map(
-					"<leader>ws",
-					require("telescope.builtin").lsp_dynamic_workspace_symbols,
-					"[W]orkspace [S]ymbols"
-				)
+				map("td", "lsp_definitions", "[T]o [D]efinitions")
+				map("tt", "lsp_type_definitions", "[T]o [T]ypes")
+				map("tr", "lsp_references", "[T]o [R]eferences")
+				map("ti", "lsp_implementations", "[T]o [I]mplementations")
+				map("<leader>sd", "lsp_document_symbols", "[S]ymbols in [D]ocument")
+				map("<leader>sw", "lsp_dynamic_workspace_symbols", "[S]ymbols in [W]orkspace")
 			end,
 		})
 	end,
