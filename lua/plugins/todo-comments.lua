5da02738c7dd3ff21e239f9bd40019f42b57b684
Artem Dragunov <dragunovartem99@gmail.com>
2025-05-06 22:37:58 +0300

    Remove non-critical plugin

--- lua/plugins/todo-comments.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"folke/todo-comments.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		require("todo-comments").setup({ signs = false })
-		vim.keymap.set("n", "<leader>ft", vim.cmd.TodoTelescope, { desc = "[F]ind [T]odos" })
-	end,
-}
