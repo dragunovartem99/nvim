a973648aa071df812e268c3fc19486b5a328e769
Artem Dragunov <dragunovartem99@gmail.com>
2025-05-06 23:16:16 +0300

    Improve conform behavior

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,8 +6,21 @@ return {
 		{
 			"<leader>F",
 			function()
-				require("conform").format()
+				-- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md
+				require("conform").format({ async = true }, function(err)
+					if not err then
+						local mode = vim.api.nvim_get_mode().mode
+						if vim.startswith(string.lower(mode), "v") then
+							vim.api.nvim_feedkeys(
+								vim.api.nvim_replace_termcodes("<Esc>", true, false, true),
+								"n",
+								true
+							)
+						end
+					end
+				end)
 			end,
+			mode = "",
 			desc = "[F]ormat Buffer",
 		},
 	},
