f384b7f1f14493a76c79fc2936844fb01d779881
Artem Dragunov <dragunovartem99@gmail.com>
2026-02-15 19:56:07 +0300

    Update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		---@diagnostic disable-next-line: missing-fields
-		require("nvim-treesitter.configs").setup({
+		require("nvim-treesitter.config").setup({
 			-- A list of parser names, or "all" (the listed parsers MUST always be installed)
 			ensure_installed = require("config.parsers.list"),
 
