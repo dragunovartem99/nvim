1ebea6dae6afa2663aa72960a2ad91e56a9450a2
Artem Dragunov <dragunovartem99@gmail.com>
2026-02-17 00:20:01 +0300

    Update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,13 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	main = "nvim-treesitter.config",
-	opts = {
-		-- A list of parser names, or "all" (the listed parsers MUST always be installed)
-		ensure_installed = require("config.parsers.list"),
-
-		sync_install = false,
-		auto_install = true,
-
-		highlight = { enable = true, additional_vim_regex_highlighting = false },
-	},
+	config = function()
+		local filetypes = require("config.parsers.list")
+		require("nvim-treesitter").install(filetypes)
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = filetypes,
+			callback = function()
+				vim.treesitter.start()
+			end,
+		})
+	end,
 }
