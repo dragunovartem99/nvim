5c40f0314bc06c5ca2c98b53a049b74ad388cab7
Artem Dragunov <dragunovartem99@gmail.com>
2026-02-19 23:49:47 +0300

    Disable semantic tokens provider

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,5 +12,12 @@ return {
 		for server_name, config in pairs(servers) do
 			vim.lsp.config(server_name, config)
 		end
+
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(args)
+				local client = vim.lsp.get_client_by_id(args.data.client_id)
+				client.server_capabilities.semanticTokensProvider = nil
+			end,
+		})
 	end,
 }
