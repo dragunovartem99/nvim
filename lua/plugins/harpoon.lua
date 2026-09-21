1d16d2433b8cd9340af18115442da6cbd1bea1db
Artem Dragunov <dragunovartem99@gmail.com>
2026-09-21 12:18:29 +0300

    Migrate harpoon to harpoon2

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,26 +1,35 @@
 return {
 	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
 	config = function()
-		local ui = require("harpoon.ui")
-		local mark = require("harpoon.mark")
+		local harpoon = require("harpoon")
+		harpoon:setup()
 
 		local map = function(keys, desc, callback)
 			vim.keymap.set("n", "<leader>" .. keys, callback, { desc = "Harpoon: " .. desc })
 		end
 
-		map("hh", "[H]arpoon", ui.toggle_quick_menu)
-		map("hm", "[H]arpoon [M]ark", mark.add_file)
+		map("hh", "[H]arpoon", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+		map("hm", "[H]arpoon [M]ark", function()
+			harpoon:list():add()
+		end)
 
-		map("hp", "[H]arpoon [P]rev", ui.nav_prev)
-		map("hn", "[H]arpoon [N]ext", ui.nav_next)
+		map("hp", "[H]arpoon [P]rev", function()
+			harpoon:list():prev()
+		end)
+		map("hn", "[H]arpoon [N]ext", function()
+			harpoon:list():next()
+		end)
 
 		local max_files = 4
 		for file = 1, max_files, 1 do
 			map("h" .. file, "[H]arpoon Open [" .. file .. "]", function()
-				ui.nav_file(file)
+				harpoon:list():select(file)
 			end)
 		end
 	end,
